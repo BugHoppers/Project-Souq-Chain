64ce329bad48bee891b281800a6a4cf1860d5aba
AdirthaBorgohain <adirthaborgohain@gmail.com>
2019-03-31 05:14:03 +0530

    Added language translation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -33,3 +33,14 @@ $(function () {
       }
   });
 });
+
+function translateLanguage(lang) {
+
+  var $frame = $('.goog-te-menu-frame:first');
+  if (!$frame.size()) {
+      alert("Error: Could not find Google translate frame.");
+      return false;
+  }
+  $frame.contents().find('.goog-te-menu2-item span.text:contains(' + lang + ')').get(0).click();
+  return false;
+}
